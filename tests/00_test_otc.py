aa4d4cdfb646a53c59790d29066fddca6fec688b
Matthias Witte <maz@g.zamiam.de>
2017-05-08 00:29:11 +0200

    Fix and extend tests

--- tests/00_test_otc.py
+++ tests/00_test_otc.py
@@ -2,12 +2,30 @@ import otc
 
 def test_shade_works():
     """Test shade works"""
-    cloud = otc.OTCCloud()
+    cloud = otc.OtcCloud(cloud='test')
 
 def test_otc():
     """Basic Test"""
-    cloud = otc.OTCCloud()
+    cloud = otc.OtcCloud(cloud='test')
     images = cloud.list_images()
     assert len(images) >= 0
+
+def test_otcclient():
+    """Test otcclient"""
+    cloud = otc.OtcCloud(cloud='test')
+    vpcs = cloud.otcclient.vpcs()
+    assert len(vpcs) >= 0
+
+def test_elb():
+    """Test otcclient"""
+    cloud = otc.OtcCloud(cloud='test')
+    elbs = cloud.otcclient.elbs()
+    assert len(elbs) >= 0
+
+def test_listeners():
+    """Test otcclient"""
+    cloud = otc.OtcCloud(cloud='test')
+    listeners = cloud.otcclient.elb_listeners()
+    assert len(listeners) >= 0
 
 # vim: sts=4 sw=4 ts=4 et:
